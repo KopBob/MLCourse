d1011dba401f5dcdd6e9520f7a6d0e99093d3c1d
Boris Kopin <kopbob@gmail.com>
2013-12-22 17:06:29 +0400

    Ex-6 is finished.

--- ex6/mlclass-ex6/ex6_spam.m
+++ ex6/mlclass-ex6/ex6_spam.m
@@ -122,10 +122,11 @@ pause;
 %  learned SVM classifier to determine whether the email is Spam or 
 %  Not Spam
 
+
 % Set the file to be read in (change this to spamSample2.txt,
 % emailSample1.txt or emailSample2.txt to see different predictions on
 % different emails types). Try your own emails as well!
-filename = 'spamSample1.txt';
+filename = 'spamSample2.txt';
 
 % Read and predict
 file_contents = readFile(filename);
