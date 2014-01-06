994f97ad6d7f77f8deae1998ffdd8c53de304b71
Boris Kopin <kopbob@gmail.com>
2014-01-06 17:23:58 +0400

    Ex-7. Task 2 is completed.

--- ex7/mlclass-ex7/plotDataPoints.m
+++ ex7/mlclass-ex7/plotDataPoints.m
@@ -4,11 +4,23 @@ function plotDataPoints(X, idx, K)
 %   PLOTDATAPOINTS(X, idx, K) plots data points in X, coloring them so that those 
 %   with the same index assignments in idx have the same color
 
+% % Create palette
+% palette = hsv(K + 1);
+% colors = palette(idx, :);
+
+% % Plot the data
+% scatter(X(:,1), X(:,2), 15, colors);
+
+subset = randperm(size(X, 1))(1:99);
+Xsubset = X(subset, :);
+idxsubset = idx(subset);
+
 % Create palette
 palette = hsv(K + 1);
-colors = palette(idx, :);
+colors = palette(idxsubset, :);
 
 % Plot the data
-scatter(X(:,1), X(:,2), 15, colors);
+
+scatter(Xsubset(:,1), Xsubset(:,2), 15, colors);
 
 end
